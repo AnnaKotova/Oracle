a58ed657990625729eea9602a48fc9c16bf6ee43
Ann Kotova <80668027682>
2015-07-21 16:38:17 +0300

    Initial Commit

---
+++ Oracle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Oracle
+//
+//  Created by Ann Kotova on 7/21/15.
+//  Copyright (c) 2015 Bmuse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
